c4848acd091907314bf05cacd5b2d06d9b4907e6
BlommamPro <orlando50002001@gmail.com>
2022-11-23 13:25:17 -0500

    Se actualizo el antiCrash

--- src/handlers/antiCrash.js
+++ src/handlers/antiCrash.js
@@ -2,17 +2,17 @@ module.exports = client => {
     process.removeAllListeners();
 
     process.on("unhandledRejection", (reason, p) =>{
-        console.log('[ANTICRASH] - ERROR ENCONTRADO');
+        console.log('[ANTICRASH] - ERROR ENCONTRADO'.bgRed);
         console.log(reason, p)
     });
 
     process.on("uncaughtException", (err, origin) =>{
-        console.log('[ANTICRASH] - ERROR ENCONTRADO');
+        console.log('[ANTICRASH] - ERROR ENCONTRADO'.bgRed);
         console.log(err, origin)
     });
 
     process.on("uncaughtExceptionMonitor", (err, origin) =>{
-        console.log('[ANTICRASH] - ERROR ENCONTRADO');
+        console.log('[ANTICRASH] - ERROR ENCONTRADO'.bgRed);
         console.log(err, origin)
     });
 
